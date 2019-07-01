0d30b84bb9c3707007e89206f5ad59d277ca0b74
idan asulin <djidanasulin@gmail.com>
2019-07-01 09:08:35 +0300

    Backend is done included bug fixes

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    DB_CONNECTION_STRING: 'mongodb://admin:Idan100asulin@ds261716.mlab.com:61716/tour-builder',
-    SENDGRID_API_KEY: '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',
-    SPOTIFY_CLIENT: 'bf4f6fdaa7404628b4ef43981b4b635d',
-    SPOTIFY_SECRET: 'd4615885339e4792973cfed9c6054199',
-    MANAGER_ACCESS_TOKEN: 'IdanasulinAviNeeman'
-}
